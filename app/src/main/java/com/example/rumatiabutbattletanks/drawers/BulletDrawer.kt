515abf97f2bcf9a3880719a77b1d8bdf0a52c4fc
Max <maz.r00721@gmail.com>
2025-05-31 09:12:30 +0300

    Initial commit

--- app/src/main/java/com/example/rumatiabutbattletanks/drawers/BulletDrawer.kt
+++ app/src/main/java/com/example/rumatiabutbattletanks/drawers/BulletDrawer.kt
@@ -75,15 +75,9 @@ class BulletDrawer(val container: FrameLayout) {
         elementsOnContainer: MutableList<Element>,
         detectedCoordinatesList: List<Coordinate>
     ) {
-        if (checkContainersContainsElements(
-                elementsOnContainer.map { it.coordinate },
-                detectedCoordinatesList
-            )
-        ) {
-            detectedCoordinatesList.forEach {
-                val element = getElementByCoordinates(it, elementsOnContainer)
-                removeElementsAndStopBullet(element, elementsOnContainer)
-            }
+        detectedCoordinatesList.forEach {
+            val element = getElementByCoordinates(it, elementsOnContainer)
+            removeElementsAndStopBullet(element, elementsOnContainer)
         }
     }
 
@@ -91,9 +85,22 @@ class BulletDrawer(val container: FrameLayout) {
         element: Element?,
         elementsOnContainer: MutableList<Element>
     ) {
+        if (element != null) {
+            if (element.material.bulletCanGoThrough) {
+                return
+            }
+            if (element.material.simpleBulletCanDestroy) {
+                stopBullet()
+                removeView(element)
+                elementsOnContainer.remove(element)
+            } else {
+                stopBullet()
+            }
+        }
+    }
+
+    private fun stopBullet() {
         canBulletGoFurther = false
-        removeView(element)
-        elementsOnContainer.remove(element)
     }
 
     private fun removeView(element: Element?) {
@@ -104,18 +111,6 @@ class BulletDrawer(val container: FrameLayout) {
             }
         }
     }
-
-    private fun checkContainersContainsElements(
-        elementsOnContainer: List<Coordinate>,
-        detectedCoordinatesList: List<Coordinate>
-    ): Boolean {
-        detectedCoordinatesList.forEach {
-            if (elementsOnContainer.contains(it)) {
-                return true
-        }
-    }
-    return false
-}
 
     private fun getCoordinatesForTopOrBottomDirection(bulletCoordinate: Coordinate): List<Coordinate> {
         val leftCell = bulletCoordinate.left - bulletCoordinate.left % CELL_SIZE
--- app/src/main/java/com/example/rumatiabutbattletanks/enums/Material.kt
+++ app/src/main/java/com/example/rumatiabutbattletanks/enums/Material.kt
@@ -1,8 +1,12 @@
 package com.example.rumatiabutbattletanks.enums
 
-enum class Material(val tankCanGoThrough: Boolean) {
-    EMPTY(true),
-    BRICK(false),
-    CONCRETE(false),
-    GRASS(true),
+enum class Material(
+    val tankCanGoThrough: Boolean,
+    val bulletCanGoThrough: Boolean,
+    val simpleBulletCanDestroy: Boolean
+) {
+    EMPTY(true, bulletCanGoThrough = true, simpleBulletCanDestroy = true),
+    BRICK(false, bulletCanGoThrough = false, simpleBulletCanDestroy = true),
+    CONCRETE(false, bulletCanGoThrough = false, simpleBulletCanDestroy = false),
+    GRASS(true, bulletCanGoThrough = true, simpleBulletCanDestroy = false),
 }
